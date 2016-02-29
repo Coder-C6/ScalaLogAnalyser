50b895e2220ed673bdf7541a14b9e79947dc59eb
Coder-C6 <Coder-C6@users.noreply.github.com>
2016-02-29 17:27:42 +0530

    Component stack
    Finished prototyping component stack

--- LogAnalyser.scala
+++ LogAnalyser.scala
@@ -1,6 +1,7 @@
 package org.Analyser.Log
 
 import java.io.File
+import scala.collection.mutable.ArrayBuffer
 import scala.util.{Failure, Success}
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -9,13 +10,11 @@ object LogAnalyser {
     //initialize
     readRecord()
   }
-
   /**
     * A sample record:
     * 1447443388.898657 0000411E:0000295E HYPTRACT: -> Delegator.getSysInfoData
     * <-----f1--------> <------f2-------> <--f3-->  f4 <----------f5----------->
     */
-
   // field 1 : only digits : some time format
   private val f1 = "([0-9]{10}.[0-9]{6})"
 
@@ -27,21 +26,21 @@ object LogAnalyser {
 
   // field 4 : '->' or '--' or '<-'
   private val f4 = "(<?[-]-?>?)"
-
   // field 5 : function message : any number of any character, reluctant
-  private val f5 = "(.*?)"
+  private val f5 = "(.+)"
 
   private val regex = s"$f1 $f2 $f3 $f4 $f5".r
 
-  val key: List[String] = List("Exception", "Failure")
-
+  var dex: Int=0
+  val key: List[String] = List("Exception", "Failure", "Error", "exception", "failure", "error")
+  var comStack = new ArrayBuffer[String]
 
   def readRecord() {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
     val sc = new SparkContext(conf)
     print("Enter file Location : ")
-    val con = Console.readLine
-    println("Thanks, you just typed: " + con)
+    var con = Console.readLine
+    con="C:\\test\\test.txt"
     val fileTemp = new File(con)
     if (fileTemp.exists()) {
 
@@ -49,27 +48,66 @@ object LogAnalyser {
       val record = sc.textFile(con).map(str =>
         regex.findFirstMatchIn(str).map(m => LogRecord(m.group(1), m.group(2), m.group(3), m.group(4), m.group(5))) match
         {
-          case Some(l) => Success(l)
+          case Some(l) => {
+            Success(l)
+          }
           case None => Failure(new Exception(s"Non matching input: $str"))
         })
 
       val cFormat = record.filter(_.isSuccess) //correct format
       val wFormat = record.filter(_.isFailure) //wrong format
 
-      record.unpersist(); // remove the rdd from memory
+      wFormat.collect().foreach(println)
 
-      //collect records with error messages
-      val messagesWithError = cFormat.collect {
-        case Success(l@LogRecord(_, _, _, _, m)) if m.contains(key) => l
+      val par = cFormat.collect {
+        case Success(l@LogRecord(t, p, c, a, m)) => {
+          parser(t,p,c,a,m)
+          Nil
+        }
       }
-      //collect records with entry point but no exit point
-      //.....
-      //build component stack
-      //......
+      par.collect()
     }
-    else {
+    else
+    {
       println("Invalid or non existing file path")
     }
+  }
+
+  def parser(t: String, p: String, c: String, a: String, m: String): Unit=
+  {
+    dex=comStack.lastIndexOf(c)
+    var error: Boolean=false
+    if(a.contentEquals("->"))
+      {
+        comStack += c
+      }
+    else if(a.contentEquals("--"))
+      {
+       //nothing
+      }
+    else if(a.contentEquals("<-"))
+      {
+        if(dex != -1)
+          {
+            comStack.remove(dex)
+          }
+        else
+        {
+          error=true
+        }
+      }
+    if(key.exists(m.contains)||(error==true))
+      {
+        //build component stack
+        comStack.foreach(print)
+      }
+
+    // to test the contents of the stack on every iteration
+    /**
+      * comStack.foreach(print)
+      * print("\n")
+      */
+
   }
 
   case class LogRecord
@@ -80,5 +118,4 @@ object LogAnalyser {
     Arrow: String,
     Message: String
   )
-
 }
